cb7304f3b47f4de957c7bf7a4aa229b4f8213fdb
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-03-06 10:53:26 -0300

    Update navigation and dependencies

--- App.jsx
+++ App.jsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Screen name="Article" component={Article} />
+      </Drawer.Navigator>
+
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
