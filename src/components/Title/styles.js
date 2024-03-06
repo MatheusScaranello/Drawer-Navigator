2fcd68a977be4b88820cbfb744f131dba9347194
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-03-06 10:49:00 -0300

    first commit

---
+++ src/components/Title/styles.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Title = () => {
+  return (
+    <View>
+      <Text>Hello, I am your Title!</Text>
+    </View>
+  );
+};
+
+export default Title;
